f899cb68cea4ecf234348f762503f0439851d265
kimeunkyu <dmsrbrjek1@naver.com>
2023-01-11 19:03:21 +0900

    2023-01-11

--- workspace/CollectionPrj/src/com/newlecture/app/util/Program.java
+++ workspace/CollectionPrj/src/com/newlecture/app/util/Program.java
@@ -1,21 +1,42 @@
 package com.newlecture.app.util;
 
-public class Program {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class Program {
+	//List : 매개변수가 식별자로써작동 (중복o)
+	//Set : 매개변수로 들어가는 값이곧 식별자(중복X)
 	public static void main(String[] args) {
-		ObjectList list = new ObjectList();
-		list.add(3);
-		list.add(5);
-		int size = list.size();
-		System.out.printf("size : %d\n", size);
+//		GList<Integer> list = new GList<>();
+		List<Integer> list = new ArrayList<>();
+		Set<Integer> set = new HashSet<>();
+		Map<String, Integer> map = new HashMap<>();
+		try {
+			list.add(3);
+			list.add(5);
+			int size = list.size();
+			System.out.printf("size : %d\n", size);
+			
+			list.clear();
+			size = list.size();
+			System.out.printf("size : %d\n", size);
+			
+			list.add(7);
+			int num = list.get(1);
+			System.out.printf("size : %d\n", num);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
-		list.clear();
-		size = list.size();
-		System.out.printf("size : %d\n", size);
-		
-		list.add(7);
-		int num =  (Integer) list.get(1);
-		System.out.printf("size : %d\n", num);
+		map.put("1번", 350);
+		map.put("2번", 250);
+		map.put("3번", 350);
+		System.out.println(map.get("2번"));
 
 
 	}
